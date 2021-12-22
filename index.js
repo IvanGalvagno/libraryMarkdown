3654dfaf00779de5184653237b25aaf3d094e893
Ivan Galvagno <ivan.galvagno@gmail.com>
2021-12-22 20:15:03 -0300

    Import bibloteca de marcacao de texto no console

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log('Here we go!');
+import chalk from 'chalk';
+
+console.log(chalk.blue('Hello world!'));
+const paragrafo = 'Texto retornado por uma function';
+
+function texto(string) {
+    return string;
+}
+
+console.log(texto(paragrafo));

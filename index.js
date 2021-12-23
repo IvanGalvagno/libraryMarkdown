ec56ccdf07782a3a64f64da9f5e1d98c839334bd
Ivan Galvagno <ivan.galvagno@gmail.com>
2021-12-23 20:55:49 -0300

    Promise

--- index.js
+++ index.js
@@ -27,3 +27,22 @@ function getFileAsyncWithPromises(filePath){
 
 getFileAsyncWithPromises('./arquivos/texto1.md');
 // getFile('./arquivos/texto1.md')
+
+
+function promessa(bool) {
+    const x = bool;
+    return new Promise((resolve, reject) => {
+      if (!x) {
+        reject(new Error("falha na promessa"));
+      }
+      resolve("sucesso na promessa");
+    });
+   }
+   
+   function exibeResposta(textoResult) {
+    console.log(textoResult);
+   }
+   
+   promessa(false)
+    .then((texto) => exibeResposta(texto))
+   // sucesso na promessa

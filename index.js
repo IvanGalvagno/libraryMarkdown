5ea674282df90a5be97e563809e18d5a86cc5ead
Ivan Galvagno <ivan.galvagno@gmail.com>
2021-12-23 20:33:33 -0300

    Using Async with Promises

--- index.js
+++ index.js
@@ -5,17 +5,25 @@ console.log(chalk.blue('Hello world!'));
 
 
 function errorHandler(pError){
-    throw new Error(chalk.white.bgRedBright(pError.code, 'field not found'));
+    throw new Error(chalk.white.bgRedBright(pError.code, 'ops'));
 }
 
-function getFile(filePath) {
-    const encoding = 'utf-8';
-    fs.readFile(filePath, encoding, (error, data)=> {
-        if (error)
-            errorHandler(error);
-        console.log(chalk.black.bgGreen.bold('Success'));
-        console.log(chalk.magentaBright(data));
-    })
+// function getFile(filePath) {
+//     const encoding = 'utf-8';
+//     fs.readFile(filePath, encoding, (error, data)=> {
+//         if (error)
+//             errorHandler(error);
+//         console.log(chalk.black.bgGreen.bold('Success'));
+//         console.log(chalk.magentaBright(data));
+//     })
+// }
+
+function getFileAsyncWithPromises(filePath){
+    const encoding = 'utf-8'
+    fs.promises.readFile(filePath, encoding)
+    .then((data) => console.log(chalk.green(data)))
+    .catch((error) => errorHandler(error))
 }
 
-getFile('./arquivos/texto1.md')
+getFileAsyncWithPromises('./arquivos/texto1.md');
+// getFile('./arquivos/texto1.md')

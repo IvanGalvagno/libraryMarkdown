4833891a0ce705f90803f182d9503fb4f10d5a0d
Ivan Galvagno <ivan.galvagno@gmail.com>
2021-12-26 18:42:10 -0300

    Get File and extracted DATA from it

--- index.js
+++ index.js
@@ -1,15 +1,35 @@
 import chalk from 'chalk';
+import fs from 'fs'
 
-const myText = 'São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/DataTransfer) utilizado em operações de arrastar e soltar, ou a partir da API `mozGetAsFile()` em um [HTMLCanvasElement](https://developer.mozilla.org/pt-BR/docs/Web/API/HTMLCanvasElement). Em Gecko, códigos com privilégiios podem criar objetos File representando qualquer arquivo local sem a intereção do usuário (veja [Implementation notes](https://developer.mozilla.org/pt-BR/docs/Web/API/File#implementation_notes) para mais informações.)'
-
-function extractLinks(text){
-    const regex = /\[[^\]]*\]\(http?s:\/\/[^$#\s].[^\s]*\)/gm;
-    // Retorna com um Array com a expressao regular escolhida, que seria os links e tags[]
-    const extractedLinks = text.match(regex);
-    extractedLinks.forEach(element => {
-        console.log(chalk.bgMagenta.white(element));
-    });
-   // console.log(chalk.bgCyanBright.gray(extractedLinks));
+// Handle the error message
+function errorHandler(pError){
+    throw new Error(chalk.white.bgRedBright(pError.code, 'ops'));
 }
 
-extractLinks(myText);
+//Extract the links from text
+function extractLinks(text){
+    const regex = /\[([^\]]*)\]\((http?s:\/\/[^$#\s].[^\s]*)\)/gm;
+    
+    const arrayWithResult = []
+    let aux;
+    // Retorna com um Array com a expressao regular escolhida, que seria os links e tags[]
+    while ((aux = regex.exec(text)) !== null) {
+        arrayWithResult.push({[aux[1]]: aux[2]});
+    }
+    return arrayWithResult;
+}
+
+//Get the file from path then readed using promise async/await and print the result
+async function getFile(filePath){
+    const encoding = 'utf-8';
+    try {
+        const data = await fs.promises.readFile(filePath, encoding)
+        console.log(extractLinks(data));
+    } catch (error) {
+        errorHandler(error)
+    } finally{
+        console.log(chalk.yellow('operação concluída'));
+    }
+}
+
+getFile('./arquivos/texto1.md')

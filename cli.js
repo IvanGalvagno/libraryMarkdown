fd83b469b8bc35e8b978bd14bd18f052f54f45be
Ivan Galvagno <ivan.galvagno@gmail.com>
2021-12-28 05:34:52 -0300

    Assembling Object(status,links), Error Handler

--- cli.js
+++ cli.js
@@ -1,7 +1,6 @@
 import chalk from 'chalk';
 import validatedURLs from './http-validacao.js';
 import getFile from './index.js'
-// import validatedURLs from './http-validacao';
 const pathX = process.argv;
 
 // On CMD you  got to type node .\cli.js .\arquivos\text1.md or the directorie
--- http-validacao.js
+++ http-validacao.js
@@ -1,21 +1,38 @@
 import fetch from 'node-fetch';
 
-async function checkStatus(arrayURLs){
-    const arrayStatus = await Promise.all(arrayURLs.map(async url =>{
-        const response = await fetch(url)
-        return response.status;
-    }));
-    return arrayStatus;
+function handlerError(e) {
+    throw new Error(e.message);
 }
 
-function generateArrayURLs(arrayLinks){
-    return arrayLinks.map(objectLink => Object.values(objectLink).join());
+
+async function checkStatus(arrayURLs) {
+    try {
+        const arrayStatus = await Promise
+            .all(arrayURLs
+                .map(async url => {
+                    const response = await fetch(url)
+                    return response.status;
+                }));
+        return arrayStatus;
+    } catch (e) {
+        handlerError(e);
+    }
 }
 
-async function validatedURLs(arrayLinks){
+function generateArrayURLs(arrayLinks) {
+    return arrayLinks
+        .map(objectLink => Object
+            .values(objectLink).join());
+}
+
+async function validatedURLs(arrayLinks) {
     const links = generateArrayURLs(arrayLinks);
     const statusLink = await checkStatus(links);
-    return statusLink;
+    const results = arrayLinks.map((object, index) => ({
+        ...object,
+        status: statusLink[index]
+    }))
+    return results;
 }
 
 export default validatedURLs;
